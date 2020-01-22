231f79028b79152fd2c6c032ef4e7f34f3f19dbf
Mauri <mauriziobella@gmail.com>
2020-01-22 16:09:26 -0500

    first commit

---
+++ src/test/java/com/example/producingwebservice/ProducingWebServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.producingwebservice;
+/*
+Started from here https://spring.io/guides/gs/producing-web-service/
+ */
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducingWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
